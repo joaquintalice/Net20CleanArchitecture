888e466de7b0c4d892cd9fb30f9218fe7a7e40c7
Joaquin <joaquintalice@gmail.com>
2024-01-23 03:11:49 -0300

    feat: UseCases - CreateOrderInteractor

---
+++ NorthWind.Sales.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -0,0 +1,35 @@
+﻿namespace NorthWind.Sales.UseCases.CreateOrder
+{
+    public class CreateOrderInteractor : ICreateOrderInputPort
+    {
+        readonly ICreateOrderOutputPort _outputPort;
+        readonly INorthWindSalesCommandsRepository _repository;
+
+        public CreateOrderInteractor(ICreateOrderOutputPort outputPort, INorthWindSalesCommandsRepository repository)
+        {
+            _outputPort = outputPort;
+            _repository = repository;
+        }
+
+        public async ValueTask Handle(CreateOrderDTO orderDTO)
+        {
+            OrderAggregate Order = new OrderAggregate()
+            {
+                CustomerId = orderDTO.CustomerId,
+                ShipAddress = orderDTO.ShipAddress,
+                ShipCity = orderDTO.ShipCity,
+                ShipCountry = orderDTO.ShipCountry,
+                ShipPostalCode = orderDTO.ShipPostalCode
+            };
+
+            foreach (var item in orderDTO.OrderDetails)
+            {
+                Order.AddDetail(item.ProductId, item.UnitPrice, item.Quantity);
+            }
+
+            await _repository.CreateOrder(Order);
+            await _repository.SaveChanges();
+            await _outputPort.Handle(Order.Id);
+        }
+    }
+}
