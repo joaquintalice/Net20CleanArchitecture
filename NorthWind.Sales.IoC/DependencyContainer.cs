0c175dd07d473df2122c72211e8d1d1a01991095
Joaquin <joaquintalice@gmail.com>
2024-01-24 23:50:21 -0300

    feat: Inversion of Control

---
+++ NorthWind.Sales.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace NorthWind.Sales.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindSalesServices(
+            this IServiceCollection services, IConfiguration configuration, string connectionStringName)
+        {
+            services
+                .AddRepositories(configuration, connectionStringName)
+                .AddUseCasesServices()
+                .AddPresenters()
+                .AddNorthWindSalesControllers();
+
+            return services;
+        }
+    }
+}
